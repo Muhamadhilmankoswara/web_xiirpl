5f7888a5d0c4ae2629a547c2ad154faf99f8cc26
Muhamadhilmankoswara <mhmdhilman710@gmail.com>
2022-10-20 08:41:05 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website XII RPL</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>hhaaa</li>
+            <li>huhusa</li>
+            <li>evos badut</li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
